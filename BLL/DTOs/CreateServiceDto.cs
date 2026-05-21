2945311f5b0197efdde3ba1920ed1eb065e78dea
flamylight <y.dmytrenko11@gmail.com>
2026-05-21 12:51:37 +0300

    feat(service): add validation attributes to CreateServiceDto

--- BLL/DTOs/CreateServiceDto.cs
+++ BLL/DTOs/CreateServiceDto.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BLL.DTOs;
 
 public class CreateServiceDto
 {
+    [Required(ErrorMessage = "Name is required")]
+    [MinLength(3, ErrorMessage = "Name must be at least 3 characters long")]
+    [MaxLength(20, ErrorMessage = "Name must be at most 20 characters long")]
     public required string Name { get; set; }
+    
+    [Required(ErrorMessage = "Description is required")]
+    [MinLength(10, ErrorMessage = "Description must be at least 10 characters long")]
+    [MaxLength(1000, ErrorMessage = "Description must be at most 1000 characters long")]
     public required string Description { get; set; }
+    
+    [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number")]
     public decimal Price { get; set; }
 }
