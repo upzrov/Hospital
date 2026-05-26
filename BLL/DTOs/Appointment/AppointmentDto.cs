44076a918562536befa71fa1078d01735bce1741
flamylight <y.dmytrenko11@gmail.com>
2026-05-26 13:47:45 +0300

    refactor(appointment): remove status field from appointment

--- BLL/DTOs/Appointment/AppointmentDto.cs
+++ BLL/DTOs/Appointment/AppointmentDto.cs
@@ -5,7 +5,6 @@ public class AppointmentDto
     public int AppointmentId { get; set; }
     public DateTime StartAt { get; set; }
     public DateTime EndAt { get; set; }
-    public string? Status { get; set; }
 
     public int DoctorId { get; set; }
     public int PatientId { get; set; }
---
+++ DAL/Migrations/20260526104310_RemoveAppointmentStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAppointmentStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Appointments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Appointments",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
