a3d4b8372ac385cd5bec70de6042cce28f03b406
flamylight <y.dmytrenko11@gmail.com>
2026-05-28 16:25:43 +0300

    feat(auth): add validation attributes to signup dto

--- BLL/DTOs/Auth/SignupDto.cs
+++ BLL/DTOs/Auth/SignupDto.cs
@@ -1,12 +1,29 @@
-﻿namespace BLL.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DTOs
 {
     public class SignupDto
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public required string Email { get; set; }
+        
         public required string Password { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters long")]
+        [MaxLength(20, ErrorMessage = "Name must be at most 20 characters long")]
         public required string Name { get; set; }
+        
+        [Required(ErrorMessage = "Full name is required")]
+        [MinLength(3, ErrorMessage = "Full name must be at least 3 characters long")]
+        [MaxLength(40, ErrorMessage = "Full name must be at most 20 characters long")]
         public required string FullName { get; set; }
+        
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\+?\d{10,15}$", ErrorMessage = "Invalid phone number")]
         public required string PhoneNumber { get; set; }
+        
+        [Required(ErrorMessage = "Date of birth is required")]
         public DateTime DateOfBirth { get; set; }
     }
 }
