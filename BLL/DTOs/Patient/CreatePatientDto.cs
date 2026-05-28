c3a61454a895ee71a6d8b036a84550f660f741b4
flamylight <y.dmytrenko11@gmail.com>
2026-05-28 16:29:13 +0300

    feat(Patient): add validation attributes to create patient dto

--- BLL/DTOs/Patient/CreatePatientDto.cs
+++ BLL/DTOs/Patient/CreatePatientDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BLL.DTOs;
 
 public class CreatePatientDto
 {
+    [Required(ErrorMessage = "Full name is required")]
+    [MinLength(3, ErrorMessage = "Full name must be at least 3 characters long")]
+    [MaxLength(40, ErrorMessage = "Full name must be at most 20 characters long")]
     public required string FullName { get; set; }
+    
     public DateTime DateOfBirth { get; set; }
+    
+    [Required(ErrorMessage = "Phone number is required")]
+    [RegularExpression(@"^\+?\d{10,15}$", ErrorMessage = "Invalid phone number")]
     public string? PhoneNumber { get; set; }
     public string? UserId { get; set; }
 }
