59e003157e2331e2fa76abf3196e513e9c46ca88
flamylight <y.dmytrenko11@gmail.com>
2026-06-07 12:53:20 +0300

    fix: convert appointment time to local timezone

--- BLL/Mapping/AppointmentProfile.cs
+++ BLL/Mapping/AppointmentProfile.cs
@@ -8,6 +8,10 @@ public class AppointmentProfile : Profile
 {
     public AppointmentProfile()
     {
-        CreateMap<Appointment, AppointmentDto>();
+        CreateMap<Appointment, AppointmentDto>()
+            .ForMember(dest => dest.StartAt, 
+                opt => opt.MapFrom(src => src.StartAt.ToLocalTime()))
+            .ForMember(dest => dest.EndAt, 
+                opt => opt.MapFrom(src => src.EndAt.ToLocalTime()));
     }
 }
